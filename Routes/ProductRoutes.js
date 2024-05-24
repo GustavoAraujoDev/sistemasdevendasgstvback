9480b0fda013d399c1b636eed06084af01f71ccf
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-05-24 00:00:24 -0300

    versao 2.0

--- Routes/ProductRoutes.js
+++ Routes/ProductRoutes.js
@@ -5,66 +5,14 @@ function requestProdutos(req, res) {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    controller.search((result) => {
-        res.write(JSON.stringify(result));
-        res.end();
-    });
-
     if (req.method === "GET") {
-        if (req.url === "/totalProdutos") {
-            // Obter informações de visão geral
-            controller.getProductCount((ProductCount) => {
-                res.write(JSON.stringify({ ProductCount }));
-                res.end();
-            });
-        }
-        // Adicione aqui a lógica para outras informações de visão geral, como número de clínicas e valor total de despesas
+        controller.Listar(res, req)
     } else if (req.method === "POST") {
-            let body = "";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
-        req.on("end", () => {
-            const parsedBody = JSON.parse(body);
-            console.log(parsedBody);
-            controller.insertData.run(
-                parsedBody.Nome,
-                parsedBody.Descricao,
-                parsedBody.Preco,
-                parsedBody.PrecoVenda,
-                parsedBody.Quantidade
-            );
-            console.log("Dados criados com sucesso.");
-        });
+        controller.Create(req, res)
     } else if (req.method === "DELETE") {
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
-        req.on("end", () => {
-            const parsedBody = JSON.parse(body);
-            console.log(parsedBody);
-            controller.deleteData.run(parsedBody.ProductID);
-            console.log("Dados excluídos com sucesso.");
-        });
+        controller.Delete(req, res)
     } else if (req.method === "PUT") {
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
-        req.on("end", () => {
-            const parsedBody = JSON.parse(body);
-            console.log(parsedBody);
-            controller.modifyData.run(
-                parsedBody.Nome,
-                parsedBody.Descricao,
-                parsedBody.Preco,
-                parsedBody.PrecoVenda,
-                parsedBody.Quantidade,
-                parsedBody.ProductID
-            );
-            console.log("Dados modificados com sucesso.");
-        });
+        controller.Update(req, res)
     }
 }
 
