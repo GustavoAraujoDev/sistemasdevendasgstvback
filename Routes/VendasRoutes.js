32fd90a48fbfbb9b8b656758bdfe93f8353ff3d0
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-07-03 14:00:30 -0300

    versao 3.0

--- Routes/VendasRoutes.js
+++ Routes/VendasRoutes.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const validateRequest = require('../middleware/validateRequest');
 const { salesSchema } = require('../middleware/salesValidation');
 
-router.get('/vendas/total', salesController.getTotalVendas);
-router.post('/vendas', validateRequest(salesSchema), salesController.inserirVenda);
-router.get('/vendas', salesController.listarVendas);
-router.get('/vendas/:vendaId/itens', salesController.listarItensVenda);
-router.put('/vendas/:vendaId', salesController.modificarVenda);
-router.delete('/vendas/:vendaId', salesController.excluirVenda);
+router.get('/total', salesController.getTotalVendas);
+router.post('/', validateRequest(salesSchema), salesController.inserirVenda);
+router.get('/', salesController.listarVendas);
+router.get('/:vendaId/itens', salesController.listarItensVenda);
+router.put('/:VendaId', salesController.modificarVenda);
+router.delete('/:VendaId', salesController.excluirVenda);
 
 module.exports = router;
