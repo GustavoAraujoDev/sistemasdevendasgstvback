87496aefc9d31ed543bcef45bd0235f4acc109d1
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-07-02 22:56:21 -0300

    versao 3.0

--- Routes/ClienteRoutes.js
+++ Routes/ClienteRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const clientController = require('../controllers/clientcontroller');
+const clientController = require('../controllers/clientController');
 const validateRequest = require('../middleware/validateRequest');
 const { clientSchema } = require('../middleware/clientValidation');
 
