f1d36c24dd510fefdd4f3682760453e540ca31ee
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-07-29 09:31:17 -0300

    versao 1.0

--- Routes/VendasRoutes.js
+++ Routes/VendasRoutes.js
@@ -8,7 +8,7 @@ router.post('/', validate(vendaSchema), vendaController.criarVenda);
 router.get('/', vendaController.listarVendas);
 router.get('/itens', vendaController.listarItensVenda);
 router.get('/:id', vendaController.buscarVendaPorId);
-router.get('/:id', vendaController.buscarItensVendaPorId);
+router.get('/itens/:id', vendaController.buscarItensVendaPorId);
 router.put('/:id', validate(vendaSchema), vendaController.atualizarVenda);
 router.delete('/:id', vendaController.deletarVenda);
 
