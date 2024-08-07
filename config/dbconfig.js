bdf1edf50f9918063c1ed2d70a205fe139a8e896
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-08-06 21:20:07 -0300

    versao 1.0

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,12 +1,26 @@
-const sqlite3 = require("sqlite3").verbose();
-const logger = require("./logger");
-const { Sequelize } = require('sequelize');
+const fs = require('fs');
 const path = require('path');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: path.join(__dirname,  '../SistemaVendaGstv.db'),
+    storage: path.join(__dirname, '../SistemaVendaGstv.db'),
 });
+
+const controlFilePath = path.join(__dirname, 'db_initialized.txt');
+
+if (!fs.existsSync(controlFilePath)) {
+    sequelize.sync({ force: true }) // Se deseja forçar a recriação das tabelas
+        .then(() => {
+            console.log('Database synchronized');
+            fs.writeFileSync(controlFilePath, 'Database initialized.');
+        })
+        .catch((err) => {
+            console.error('Error synchronizing database:', err);
+        });
+} else {
+    console.log('Database already initialized.');
+}
 
 module.exports = sequelize;
